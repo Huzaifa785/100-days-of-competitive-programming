7ac5a7abb9666358c9c0858401f4fe5ee0186e11
Huzaifa Mohammed <huzaifa.coder785@gmail.com>
2021-07-10 23:08:21 -0700

    Initial Commit

---
+++ leap-year.js
@@ -0,0 +1,24 @@
+// Problem Statement 👇
+/*
+Given a year, find whether leap year or not?
+
+Input Description:
+Given a 4 digit number
+
+Output Description:
+Print leap year or not a leap year
+
+Sample Input :
+1996
+Sample Output :
+leap year
+---------------------------------------------------------------------*/
+
+// Solution --> Solved using JavaScript 👇
+year = 2020;  
+if (0 === year % 4) {
+    console.log('leap year');
+} 
+else {
+    console.log('not a leap year')
+};
