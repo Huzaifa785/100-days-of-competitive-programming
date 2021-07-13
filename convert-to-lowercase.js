1ddae295d2493b25af59e9a6995d8658ec24477a
Huzaifa Mohammed <huzaifa.coder785@gmail.com>
2021-07-12 23:49:51 -0700

    Initial Commit

---
+++ convert-to-lowercase.js
@@ -0,0 +1,18 @@
+/*------------------------------Problem Statement----------------------------------
+Convert the given string into lower case
+
+Input Description:
+You'll be given a string
+
+Output Description:
+Print the string into lower case
+
+Sample Input :
+Node JS
+Sample Output :
+node js
+-----------------------------------------------------------------------------------*/
+
+// Solution
+let input_str = "Node JS"
+console.log(input_str.toLowerCase())
